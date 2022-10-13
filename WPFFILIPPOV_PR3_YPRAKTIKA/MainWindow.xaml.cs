a8a00349fa544e7f232a2c1a8dad6cf92b42c93a
NekoYo1 <kostyafear203@gmail.com>
2022-10-13 17:17:59 +1000

    Кнопка перехода на сам работу

--- WPFFILIPPOV_PR3_YPRAKTIKA/MainWindow.xaml.cs
+++ WPFFILIPPOV_PR3_YPRAKTIKA/MainWindow.xaml.cs
@@ -24,5 +24,11 @@ namespace WPFFILIPPOV_PR3_YPRAKTIKA
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Window1 window1 = new Window1();
+            window1.Show();
+        }
     }
 }
